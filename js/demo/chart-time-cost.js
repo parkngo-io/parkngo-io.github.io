60874c50ad149660e2f308edf6a46e5345fe2f40
amalrkrishna <ioparkngo@gmail.com>
2018-12-20 16:12:06 -0500

    dashboard

--- js/demo/chart-time-cost.js
+++ js/demo/chart-time-cost.js
@@ -9,22 +9,23 @@ var myLineChart = new Chart(ctx, {
   data: {
     labels: ["New York", "Los Angeles", "San Francisco", "Washington D.C", "Seattle", "Chicago", "Boston", "Atlanta", "Dallas", "Detroit"],
     datasets: [
-      { type: 'line',
-      fill: false,
-      lineTension: 0,
-      label: "Annual search cost ($)",
-      backgroundColor: "rgb(170, 28, 59)",
-      borderColor: "rgb(170, 28, 59)",
-      yAxisID: 'B',
-      data: [2243, 1785, 1735, 1367, 1205, 1174, 1111, 1043, 995, 731],
-    },
       {
-      label: "Annual search time (hours)",
+        type: 'line',
+        fill: false,
+        lineTension: 0,
+        label: "2 hr parking cost ($)",
+        backgroundColor: "rgb(170, 28, 59)",
+        borderColor: "rgb(170, 28, 59)",
+
+        data: [33, 14, 12, 18, 10, 22, 26, 6, 6, 9],
+      },
+      { 
+      label: "Annual search cost ($)",
       backgroundColor: "rgb(64, 140, 203)",
       borderColor: "rgb(64, 140, 203)",
-      yAxisID: 'A',
-      data: [107, 85, 83, 65, 58, 56, 53, 50, 48, 35],
+      data: [2243, 1785, 1735, 1367, 1205, 1174, 1111, 1043, 995, 731],
     }
+
 
   ],
   },
@@ -42,31 +43,14 @@ var myLineChart = new Chart(ctx, {
         }
       }],
       yAxes: [
-        {
-        id: 'A',
-        scaleLabel: {
-          display: true,
-          labelString: 'time (hours)'
-        },
-        position: 'left',
-        ticks: {
-          min: 0,
-          max: 150,
-          maxTicksLimit: 5
-        },
-        gridLines: {
-          display: true
-        }
-      },
-      { id: 'B',
+      { 
+        type: "logarithmic",
       scaleLabel: {
         display: true,
         labelString: 'cost ($)'
       },
-      position: 'right',
+      position: 'left',
         ticks: {
-          min: 0,
-          max: 2500,
           maxTicksLimit: 5
         },
         gridLines: {
