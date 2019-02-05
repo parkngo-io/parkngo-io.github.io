e4995675d583ef9c48b9f0935020af393a6de000
Amal Krishna R <amalrkrishna94@gmail.com>
2019-02-04 23:27:02 -0500

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "https://formspree.io/ioparkngo@gmail.com",
+        url: "//formspree.io/ioparkngo@gmail.com",
         type: "POST",
         data: {
           name: name,
