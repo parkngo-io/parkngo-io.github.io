c6b14d1808528e9095eacffcd7a6b17d26549fea
amalrkrishna <ioparkngo@gmail.com>
2019-02-07 03:01:28 -0500

    save

--- js/newsletter.js
+++ js/newsletter.js
@@ -21,27 +21,6 @@
     doc.setAttribute('data-useragent', navigator.userAgent);
 
 
-   /* Preloader
-    * -------------------------------------------------- */
-    var clPreloader = function() {
-        
-        $("html").addClass('ss-preload');
-
-        $WIN.on('load', function() {
-
-            //force page scroll position to top at page refresh
-            // $('html, body').animate({ scrollTop: 0 }, 'normal');
-
-            // will first fade out the loading animation 
-            $("#loader").fadeOut("slow", function() {
-                // will fade out the whole DIV that covers the website.
-                $("#preloader").delay(300).fadeOut("slow");
-            }); 
-       
-        });
-    };
-
-
    /* AjaxChimp
     * ------------------------------------------------------ */
     var clAjaxChimp = function() {
